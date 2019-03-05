baf87714ae75f10f3a97d976d6a9c4ab804c2d90
Rudy Ravelin <rudy@noukod.com>
2019-03-05 17:15:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ const logDriverNames = function (drivers) {
 //     }
 //   });
 // };
-// 
+//
 // const driversByRevenue = function (drivers) {
 //   return drivers.slice().sort(function (driverOne, driverTwo){
 //     return driverOne.revenue - driverTwo.revenue;
 //   });
 // };
-// 
+//
 // driversByName = function(drivers) {
 //   return drivers.slice().sort(function (driverOne, driverTwo){
 //     return driverOne.name.localeCompare(driverTwo.name);
 //   });
 // };
-// 
+//
 // const totalRevenue = function (drivers) {
 //   return drivers.reduce(function ( total, currentDriver) {
 //     return currentDriver.revenue + total;
 //   }, 0);
 // };
-// 
+//
 // const averageRevenue = function (drivers) {
 //   return totalRevenue(drivers) / drivers.length;
 // };
